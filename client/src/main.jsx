e713204585e46dedba4ecbfe68e0731781d2b019
Muhammad Khan <khanmuhammadkolachi390@gmail.com>
2026-02-28 23:00:55 +0500

    chore: add vercel speed insights for react

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <SpeedInsights />
+  </>,
+);
